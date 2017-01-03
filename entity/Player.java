e7de562d43fb87882ddfe63e93a70e61586cf64a
DeftLee <1509706396@qq.com>
2017-01-03 15:18:27 +0800

    Update Player.java

--- entity/Player.java
+++ entity/Player.java
@@ -1,1 +1,74 @@
+package graduate;
 
+public class Player {
+	private String id;
+	private String name;
+	private String height;
+	private String weight;
+	private String age;
+	private String grade;
+	private String goal;
+	private String position;
+	private String number;
+	private String role;  //Role in team, team member or caption
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getHeight() {
+		return height;
+	}
+	public void setHeight(String height) {
+		this.height = height;
+	}
+	public String getWeight() {
+		return weight;
+	}
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getGoal() {
+		return goal;
+	}
+	public void setGoal(String goal) {
+		this.goal = goal;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getRole() {
+		return this.role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
