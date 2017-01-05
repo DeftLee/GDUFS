341a217380da60c1df1636d6ee5d94b836c09a05
DeftLee <1509706396@qq.com>
2017-01-05 14:15:21 +0800

    Update PasswordManageDao.java

--- dao/PasswordManageDao.java
+++ dao/PasswordManageDao.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PasswordManageDao {
-	public boolean checkUserAndEmail(String userName,String email) {
+	public String checkUserAndEmail(String userName,String email) {
 		Connection con = DBHelper.connect();
 		PreparedStatement stat = null;
 		String sql = "select * from gdufs.user where userName='"+userName+"' and email='"+email+"';";
@@ -15,14 +15,14 @@ public class PasswordManageDao {
 			stat = con.prepareStatement(sql);
 			ResultSet rs = stat.executeQuery();
 			if(rs.next()) {
-				return true;
+				return "right";
 			}
 			else {
-				return false;
+				return "wrong";
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return false;
+			return "error";
 		} finally {
 			DBHelper.closePreparedStatement(stat);
 			DBHelper.closeConneciton(con);
@@ -48,6 +48,53 @@ public class PasswordManageDao {
 			}
 			else {
 				return "notExist";
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "error";
+		} finally {
+			DBHelper.closePreparedStatement(stat);
+			DBHelper.closeConneciton(con);
+		}
+	}
+	
+	public String getPass(String userName) {
+		Connection con = DBHelper.connect();
+		PreparedStatement stat = null;
+		String sql = "select * from gdufs.user where userName='"+userName+"';";
+		
+		try {
+			stat = con.prepareStatement(sql);
+			ResultSet rs = stat.executeQuery();
+			if(rs.next()) {
+				String pass = rs.getString("password");
+				return pass;
+			}
+			else {
+				return "error";
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "error";
+		} finally {
+			DBHelper.closePreparedStatement(stat);
+			DBHelper.closeConneciton(con);
+		}
+	}
+	
+	public String checkUserAndPass(String userName,String password) {
+		Connection con = DBHelper.connect();
+		PreparedStatement stat = null;
+		String sql = "select * from gdufs.user where userName='"+userName+"' and password='"+password+"';";
+		
+		try {
+			stat = con.prepareStatement(sql);
+			ResultSet rs = stat.executeQuery();
+			if(rs.next()) {
+				return "right";
+			}
+			else {
+				return "wrong";
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
