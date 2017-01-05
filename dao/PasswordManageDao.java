8b0e533ceac45fc5958b4bca18baaf402a454be6
DeftLee <1509706396@qq.com>
2017-01-05 09:48:58 +0800

    Update PasswordManageDao.java

--- dao/PasswordManageDao.java
+++ dao/PasswordManageDao.java
@@ -28,4 +28,33 @@ public class PasswordManageDao {
 			DBHelper.closeConneciton(con);
 		}
 	}
+	
+	public String checkName(String userName) {
+		Connection con = DBHelper.connect();
+		PreparedStatement stat = null;
+		String sql = "select * from gdufs.user where userName='"+userName+"';";
+		
+		try {
+			stat = con.prepareStatement(sql);
+			ResultSet rs = stat.executeQuery();
+			if(rs.next()) {
+				String status = rs.getString("status").trim();
+				if(status.equals("active")) {
+					return "active";
+				}
+				else {
+					return "notActive";
+				}
+			}
+			else {
+				return "notExist";
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "error";
+		} finally {
+			DBHelper.closePreparedStatement(stat);
+			DBHelper.closeConneciton(con);
+		}
+	}
 }
